ae05a7e50e68325b736d4b17502ca2ed4e17d7bc
Jure Srsen <hoornet@gmail.com>
2009-10-26 11:50:47 +0100

    Dodal stare naloge

---
+++ FORPC101/Week 5/L6E1-Unpredictable-String.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+#   Exercise1.
+#   Write a class UnpredictableString which is a sub-class of String.
+#   This sub-class should have a method called scramble() which randomly
+#   rearranges any string as follows:
+#
+#   >ruby unpredictablestring.rb
+
+#   daano.r n sdt a htIsw taikmgy r
+
+#   >Exit code: 0
+
+#   # the original string was: "It was a dark and stormy night."
+
+class UnpredictableString < String
+  def scramble()
+    
+  end
+end
---
+++ FORPC101/Week 5/cat.rb
@@ -0,0 +1,18 @@
+ # cat.rb
+ require 'animal'
+ class Cat < Animal
+
+   def initialize(breed, name)
+     @breed = breed
+     @name = name
+   end
+
+   def meow
+     "Class Cat's meow method: Meow! Meow!"
+   end
+
+   # override display
+   def display
+    "Class Cat's display method: I am of #{@breed} breed and my name is #{@name}."
+   end
+ end
