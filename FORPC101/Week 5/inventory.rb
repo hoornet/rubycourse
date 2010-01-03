376abcbcc76b80b34dab63e78798c4c212ca5ca7
hoornet <hoornet@gmail.com>
2010-01-03 13:56:20 +0100

    I am late in exercises

--- FORPC101/Week 5/inventory.rb
+++ FORPC101/Week 5/inventory.rb
@@ -12,6 +12,10 @@ at the command prompt:
 
 ruby inventory.rb > old-inventory.txt
 
+THIS PART IS DONE!!!
+
+###########################
+
 After a few days, when some files would have been added / deleted from this
 folder, run the program again like:
 
@@ -23,82 +27,8 @@ have been added in this folder since the time the old-inventory.txt was created.
 
 ###########################################################
 
-USAGE = "ruby inventory.rb >  target.txt"
+#USAGE = "ruby inventory.rb > old-inventory.txt"
 
 ################################################################################
+File.open(ARGV[0], 'w') { |f| f << Dir.glob('**/*') }
 
-
-class DirFileParser
-  def initialize
-    @files_and_dirs = Dir.glob('**/*')
-  end
-  
-  def get_file_to_params
-    @target = ARGV[0]
-
-  end
-  
-  def print_data
-    print @target
-    print @source
-    print @files_and_dirs
-  end
-end
-
-
-a = DirFileParser.new
-a.get_file_to_params
-
-
-#
-#require 'optparse'
-#
-#class CountdownOptions < OptionParser
-#  attr_reader :quiet, :show_all, :mode, :method, :source, :target
-#  
-#  def initialize
-#    super
-#    # Defaults
-#    @mode = :Fraction
-#    @method = :Recursive
-#
-#    # Switches
-#    on("--all", "-a")       do @show_all = true end
-#    on("--integral", "-i")  do @mode = :Integral end
-#    on("--recursive", "-r") do @method = :Recursive end
-#    on("--memoized", "-m")  do @method = :Memoized end
-#    on("--quiet", "-q")     do @quiet = true end
-#    
-#    @target, *@source = *parse(ARGV).map{|i|i.to_i}
-#    if @mode != :Integral
-#      @target = @target.to_f
-#      @source = @source.map { | e | e.to_f }
-#    end
-#    raise OptionParser::MissingArgument, "Not enough arguments" unless @source and source.length > 0
-#  end
-#end
-#
-#if $0 == __FILE__
-#  begin
-#    opts = CountdownOptions.new
-#    
-#    require 'countdown-recursive' rescue $stderr.puts "Recursive brute force library not found, recursive solution not available."
-#    require 'countdown-memoized' rescue $stderr.puts "Memoization library not found, memoization not available."
-#    
-#    solutions = []
-#    best = solve_countdown(opts.target, opts.source, Object.const_get(opts.method).const_get(opts.mode)) do | best |
-#      $stderr.puts "Best so far: #{best} = #{best.value}" unless opts.quiet
-#      if best.value == opts.target
-#        solutions << best
-#        break best unless opts.show_all
-#      end
-#    end
-#    if solutions.empty? or !opts.show_all
-#      puts "Best approximation for combinations of #{opts.source.inspect} to yield #{opts.target}:", "#{best} = #{best.value}"
-#    else
-#      puts "Solutions for combinations of #{opts.source.inspect} to yield #{opts.target}:", solutions.uniq
-#    end
-#  rescue OptionParser::ParseError
-#    puts "Usage", USAGE
-#  end
-#end
